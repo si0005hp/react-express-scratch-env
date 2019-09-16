0fb4312a84286ebfdca84b549099e619d3d12b14
si0005hp <si0005hp@gmail.com>
2019-09-16 20:54:49 +0900

    log

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import config from 'config';
+import { logger } from './logger';
 
 const app = express();
 
@@ -9,13 +10,13 @@ const serverConfig = config.get('server');
 app.use(express.static(path.join('./', 'dist')));
 
 app.get('/api', (req, res) => {
-  res.send({data: 'test'});
+  res.send({ data: 'test' });
 })
 
 app.get('*', function (req, res) {
   res.sendFile(path.join('./', 'dist', 'index.html'))
 })
 
-app.listen(serverConfig.port, ()=> {
-  console.log(`server starting -> [port] ${serverConfig.port} [env] ${process.env.NODE_ENV}`);
+app.listen(serverConfig.port, () => {
+  logger.info(`server starting -> [port] ${serverConfig.port} [env] ${process.env.NODE_ENV}`);
 })
