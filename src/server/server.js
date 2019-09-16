c2c2fa1552a969a8417f0ca83bf413b27e29566a
si0005hp <si0005hp@gmail.com>
2019-09-16 20:50:14 +0900

    config

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import path from 'path';
+import config from 'config';
 
 const app = express();
+
+const serverConfig = config.get('server');
 
 app.use(express.static(path.join('./', 'dist')));
 
 app.get('/api', (req, res) => {
-  res.send({api: 'test'});
+  res.send({data: 'test'});
 })
 
 app.get('*', function (req, res) {
   res.sendFile(path.join('./', 'dist', 'index.html'))
 })
 
-app.listen(3031, ()=> {
-  console.log('server running');
+app.listen(serverConfig.port, ()=> {
+  console.log(`server starting -> [port] ${serverConfig.port} [env] ${process.env.NODE_ENV}`);
 })
